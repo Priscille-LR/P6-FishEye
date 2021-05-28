a4521a80ba07e22672c42370a1e80c0f223c096b
Priscille-LR <76170675+Priscille-LR@users.noreply.github.com>
2021-05-28 13:51:37 +0200

    config gh io

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { router } from './Router';
+import { router } from './router';
 
 window.addEventListener('hashchange', router); // identifier of the URL changes
 window.addEventListener('load', router); // ressource has loaded
